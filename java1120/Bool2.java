607c2a152e7d3268598f26b786b3798f6fa8fc1e
shlee0619 <shlee619@icloud.com>
2024-11-20 15:13:43 +0900

    first commit

--- java1120/Bool2.java
+++ java1120/Bool2.java
@@ -1,5 +1,10 @@
 package java1120;
 
 public class Bool2 {
-    
+    public static void main(String[] args) {
+        int i = 3;
+        boolean isOdd = i % 2 == 1;
+        System.out.println(isOdd);
+    }
+
 }
