b215b8ce6eeb43fe39e00e909d494acde6e41241
shlee0619 <shlee619@icloud.com>
2024-11-20 15:21:27 +0900

    test

---
+++ java1120/char_var.java
@@ -0,0 +1,19 @@
+package java1120;
+
+public class char_var{
+
+    public static void main(String[] args) {
+
+        char a1 = 'a';
+        char a2 = 97;
+        char a3 = '\u0061';
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+    
+
+    
+    
+}
